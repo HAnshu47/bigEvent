7c44fa09a109d1e74863173290c42cf606190fdb
HAnshu47 <1477315575@qq.com>
2019-09-22 20:57:45 +0800

    封装了一个地址,修改模态框

---
+++ web_back/js/utils/config.js
@@ -0,0 +1,6 @@
+var base_URL='http://localhost:8000';
+var UrlList={
+    user_login:base_URL+'/admin/login',
+    user_logout:base_URL+'/admin/login',
+    user_grtInfo:base_URL+'/admin/getuser'
+}
