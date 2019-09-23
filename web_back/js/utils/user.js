21fb8228a99fcf00e633dac0b9b19b158baad026
HAnshu47 <1477315575@qq.com>
2019-09-23 11:18:09 +0800

    后台退出操作基本完成

--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -2,28 +2,31 @@ var user={
     /**
      * 
      * @param {*} userName 用户名
-     * @param {*} password 密码
+     * @param {*} passWord 密码
      * @param {*} callBack 回调函数
      */
-    login:function(userName,password,callBack){
+    // 用户登录
+    login:function(userName,passWord,callBack){
         $.post(
             UrlList.user_login,
             {
                 user_name:userName,
-                password:password
+                password:passWord
             },
             function(res){
                 callBack(res)
             }
         );
     },
+    // 用户退出
     logout:function(callBack){
         $.post(UrlList.user_logout,function(res){
             callBack(res);
         })
     },
+    // 获取用户信息
     getInfo:function(callBack){
-        $.get(UrlList.user_grtInfo,function(res){
+        $.get(UrlList.user_getInfo,function(res){
             callBack(res)
         })
     }
